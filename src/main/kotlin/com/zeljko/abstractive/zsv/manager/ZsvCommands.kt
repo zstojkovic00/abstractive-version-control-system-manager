daa15b51f185654550c9f52b9d10ee2cb967b6ef
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-09-20 01:52:59 +0200

    testing ls-tree command v2

--- src/main/kotlin/com/zeljko/abstractive/zsv/manager/ZsvCommands.kt
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/ZsvCommands.kt
@@ -128,18 +128,24 @@ class ZsvCommands {
         }
     }
 
-    // 100644 (regular file)
-    // 100766 (executable file)
-    // 120000 (symbolic link)
-    // 040000 (directory)
+/*  100644 (regular file)
+    100766 (executable file)
+    120000 (symbolic link)
+    040000 (directory)
 
-    // directory    object       hash                                        name
-    // 040000       tree         4c2de5dff69543f68b6238e0510d420b59b334f7    main
+    directory    object       hash                                        name
+    120000       blob         541cb64f9b85000af670c5b925fa216ac6f98291    link_to_test.txt
+    040000       tree         f433240f70c738bfcc2f48994d7ca0c843a763ad    main
+    100644       blob         16e9ca692c04fe47655e06ed163cddd0f4c49687    test.sh
+    100644       blob         10500012fca9b4425b50de67a7258a12cba0c076    test.txt
+    040000       tree         81803f67f37d96f8b76ae69719ebb5e5cbcbb869    test
 
+    */
     private fun parseTreeContent(decompressedContent: String): List<Tree> {
 
         // remove <tree> <size>
         val content = decompressedContent.split("\u0000").drop(1).joinToString("")
+        val validModes = listOf("120000", "100644", "100755", "40000")
         var index = 0
 
         while(index < content.length){
