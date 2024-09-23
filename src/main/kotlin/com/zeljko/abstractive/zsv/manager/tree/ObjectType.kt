42fbe482439cd1fcc2d272a1ebe8bb9be39c1db4
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-09-23 21:22:20 +0200

    improvement: add enum class for tree object type

---
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/tree/ObjectType.kt
@@ -0,0 +1,21 @@
+package com.zeljko.abstractive.zsv.manager.tree
+
+enum class ObjectType(val mode: String) {
+
+    REGULAR_FILE("100644"),
+    EXECUTABLE_FILE("100755"),
+    SYMBOLIC_LINK("120000"),
+    DIRECTORY("40000");
+
+    companion object {
+        fun fromMode(mode: String): ObjectType {
+            return when (mode) {
+                "100644" -> REGULAR_FILE
+                "100755" -> EXECUTABLE_FILE
+                "120000" -> SYMBOLIC_LINK
+                "040000", "40000" -> DIRECTORY
+                else -> throw IllegalArgumentException("Invalid file mode: $mode")
+            }
+        }
+    }
+}
--- src/main/kotlin/com/zeljko/abstractive/zsv/manager/tree/Tree.kt
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/tree/Tree.kt
@@ -1,32 +1,37 @@
 package com.zeljko.abstractive.zsv.manager.tree
+import com.zeljko.abstractive.zsv.manager.tree.ObjectType.*
+
 
 /**
-  100644 (regular file)
-  100766 (executable file)
-  120000 (symbolic link)
-  040000 (directory)
+100644 (regular file)
+100766 (executable file)
+120000 (symbolic link)
+040000 (directory)
 
-  fileMode     objectType   objectSha                                   fileName
-  120000       blob         541cb64f9b85000af670c5b925fa216ac6f98291    link_to_test.txt
-  040000       tree         f433240f70c738bfcc2f48994d7ca0c843a763ad    main
-  100644       blob         16e9ca692c04fe47655e06ed163cddd0f4c49687    test.sh
-  100644       blob         10500012fca9b4425b50de67a7258a12cba0c076    test.txt
-  040000       tree         81803f67f37d96f8b76ae69719ebb5e5cbcbb869    test
-*/
+fileMode     objectType   objectSha                                   fileName
+120000       blob         541cb64f9b85000af670c5b925fa216ac6f98291    link_to_test.txt
+040000       tree         f433240f70c738bfcc2f48994d7ca0c843a763ad    main
+100644       blob         16e9ca692c04fe47655e06ed163cddd0f4c49687    test.sh
+100644       blob         10500012fca9b4425b50de67a7258a12cba0c076    test.txt
+040000       tree         81803f67f37d96f8b76ae69719ebb5e5cbcbb869    test
+ */
 data class Tree(val fileMode: String, val fileName: String, val objectSha: String) {
 
     override fun toString(): String {
         return String.format("%-6s %-4s %-40s %s",
             fileMode,
-            getObjectType(fileMode),
+            getObjectType(),
             objectSha,
             fileName)
     }
-    private fun getObjectType(fileMode: String): String {
-        return when (fileMode) {
-            "100644", "100755", "120000" -> "blob"
-            "040000", "40000" -> "tree"
-            else -> throw IllegalArgumentException("Invalid object type")
+
+    private val objectType: ObjectType
+        get() = ObjectType.fromMode(fileMode)
+
+    private fun getObjectType(): String {
+        return when (objectType) {
+            REGULAR_FILE, EXECUTABLE_FILE, SYMBOLIC_LINK -> "blob"
+            DIRECTORY -> "tree"
         }
     }
 }
