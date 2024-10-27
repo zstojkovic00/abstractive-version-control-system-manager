25b9cacd7eb4f1a45d58347f47519af2b3b3796f
zstojkovic00 <00zeljkostojkovic@gmail.com>
2024-10-27 16:54:07 +0100

    feature: git client can send want command for negotiation phase to tell server which objects clients want to receive

--- src/main/kotlin/com/zeljko/abstractive/zsv/manager/transport/client/GitNativeClient.kt
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/transport/client/GitNativeClient.kt
@@ -12,6 +12,9 @@ class GitNativeClient {
         private const val FLUSH_PACKET = "0000"
         private const val NUL_BYTE = '\u0000'
         private const val GIT_UPLOAD_PACK = "git-upload-pack"
+        private const val GIT_WANT = "want"
+        private const val GIT_DONE = "done"
+        private const val NEWLINE = "\n"
     }
 
     fun connect(gitUrl: GitUrl) {
@@ -20,37 +23,62 @@ class GitNativeClient {
             val output = DataOutputStream(socket.outputStream)
 
             sendRefDiscoveryRequest(output, gitUrl)
-            readServerResponses(input)
-            terminateConnection(output)
+            val references = readPacketReferenceResponse(input)
+            println(references)
+
+            val headSha = references.first().sha
+            sendWantRequest(output, headSha)
+            readPackFile(input)
         }
     }
 
     private fun sendRefDiscoveryRequest(output: DataOutputStream, gitUrl: GitUrl) {
-        val packet = createRefDiscoveryPacket(gitUrl)
+        val packet = createPacket {
+            append(GIT_UPLOAD_PACK)
+                .append(" ")
+                .append(gitUrl.path)
+                .append(NUL_BYTE)
+                .append("host=")
+                .append(gitUrl.host)
+                .append(NUL_BYTE)
+        }
         output.writeBytes(packet)
         output.flush()
     }
 
-
-    // 0032git-upload-pack /test-repo\0host=127.0.0.1\0
-    private fun createRefDiscoveryPacket(gitUrl: GitUrl): String {
-        val command = buildString {
-            append(GIT_UPLOAD_PACK)
-            append(" ")
-            append(gitUrl.path)
-            append(NUL_BYTE)
-            append("host=")
-            append(gitUrl.host)
-            append(NUL_BYTE)
+    private fun sendWantRequest(output: DataOutputStream, headSha: String) {
+        val packet = buildString {
+            append(createPacket {
+                append(GIT_WANT)
+                    .append(" ")
+                    .append(headSha)
+                    .append(NEWLINE)
+            })
+            append(FLUSH_PACKET)
+            append(createPacket {
+                append(GIT_DONE)
+                append(NEWLINE)
+            })
         }
+        output.writeBytes(packet)
+        output.flush()
+    }
 
-        val length = command.length + 4 // +4 for length header
+    private fun createPacket(buildCommand: StringBuilder.() -> Unit): String {
+        val command = buildString(buildCommand)
+        val length = command.length + 4
         val hexLength = String.format("%04x", length)
-
         return "$hexLength$command"
     }
 
-    private fun readServerResponses(input: DataInputStream) {
+
+    private fun readPackFile(input: DataInputStream) {
+        // TODO:  Read objects
+    }
+
+    private fun readPacketReferenceResponse(input: DataInputStream): List<GitReference> {
+        val responseBuilder = StringBuilder()
+
         while (true) {
             val lengthHex = input.readNBytes(4).toString(StandardCharsets.UTF_8)
             if (lengthHex == FLUSH_PACKET) break
@@ -59,16 +87,21 @@ class GitNativeClient {
             if (length > 0) {
                 val responseData = input.readNBytes(length)
                 val response = responseData.toString(StandardCharsets.UTF_8)
-                val gitReferences = parseResponse(response)
-                println(gitReferences)
+                responseBuilder.append(response)
             }
         }
+
+        if (responseBuilder.isEmpty()) {
+            return listOf()
+        }
+
+        return parseReferences(responseBuilder.toString())
     }
 
-    // f490f44a26417c31c44174e12fdbc5a53a761082 HEADmulti_ack thin-pack side-band side-band-64k ofs-delta shallow deepen-since deepen-not deepen-relative no-progress include-tag multi_ack_detailed symref=HEAD:refs/heads/master object-format=sha1 agent=git/2.34.1
+    // f490f44a26417c31c44174e12fdbc5a53a761082 HEAD\u0000multi_ack thin-pack side-band side-band-64k ofs-delta shallow deepen-since deepen-not deepen-relative no-progress include-tag multi_ack_detailed symref=HEAD:refs/heads/master object-format=sha1 agent=git/2.34.1
     // f490f44a26417c31c44174e12fdbc5a53a761082 refs/heads/master
     // f490f44a26417c31c44174e12fdbc5a53a761082 refs/remotes/origin/master
-    private fun parseResponse(response: String): List<GitReference> {
+    private fun parseReferences(response: String): List<GitReference> {
         return response.lines()
             .filter { it.isNotEmpty() }
             .map { line ->
@@ -81,10 +114,5 @@ class GitNativeClient {
                     GitReference(sha, rest)
                 }
             }
-    }
-
-    private fun terminateConnection(output: DataOutputStream) {
-        output.writeBytes(FLUSH_PACKET)
-        output.flush()
     }
 }
