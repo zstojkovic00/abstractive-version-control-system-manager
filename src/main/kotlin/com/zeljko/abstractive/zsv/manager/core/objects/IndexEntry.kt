9c6539f596b9ed0221b898ed3632a2285c2e411e
zstojkovic <00zeljkostojkovic@gmail.com>
2024-11-22 23:51:48 +0100

    feature: update index entry when file is modified

--- src/main/kotlin/com/zeljko/abstractive/zsv/manager/core/objects/IndexEntry.kt
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/core/objects/IndexEntry.kt
@@ -1,33 +1,107 @@
 package com.zeljko.abstractive.zsv.manager.core.objects
 
+import com.zeljko.abstractive.zsv.manager.utils.toHexString
+import com.zeljko.abstractive.zsv.manager.utils.toShaByte
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.BasicFileAttributes
 
-/*
-
-The index, which is a single file at .zsv/index, is stored in a custom binary format
-The first 12 bytes are the header, the last 20 a SHA-1 hash of the index,
-and the bytes in between are index entries, each 62 bytes plus the length of the path
-
-The index is a binary file that has the following format:
-
-    DIRC <version_number> <number of entries> // 12 bytes
-
-    <ctime> <mtime> <dev> <ino> <mode> <uid> <gid> <SHA> <flags> <path> //
-    <ctime> <mtime> <dev> <ino> <mode> <uid> <gid> <SHA> <flags> <path>
-    <ctime> <mtime> <dev> <ino> <mode> <uid> <gid> <SHA> <flags> <path>
-
-    # more entries
-
+/**
+ * Represents an entry in the Git index file (.zsv/index).
+ * The index is stored in a custom binary format:
+ * - First 12 bytes are header (DIRC + version + number of entries)
+ * - Followed by index entries
+ * - Each entry is 68 bytes plus variable length path name
+ * - Last 20 bytes are SHA-1 of the index
+ *
+ * Index file format:
+ * DIRC <version> <entries>
+ * <ctime> <mtime> <dev> <ino> <mode> <uid> <gid> <SHA> <flags> <path>
+ * <ctime> <mtime> <dev> <ino> <mode> <uid> <gid> <SHA> <flags> <path>
+ * ...
  */
-// 68 bytes without header and path name
 data class IndexEntry(
-    val ctime: Long, // creation/change time, nanoseconds // 8
-    val mtime: Long, // modification time, nanoseconds // 8
-    val dev: Long, // The ID of device containing this file // 8
-    val ino: Long, // the file's inode number // 8
-    val mode: Int, // 4
-    val uid: Int, // User ID of owner // 4
-    val gid: Int, // Group ID of owner // 4
-    val sha: String?, // 20
-    val flags: Int, // 4
-    val pathName: String // test.txt 8 + nul = 9
-)
+    val offset: Int?,      // Byte offset in index (not stored)
+    val ctime: Long,       // Creation time (8 bytes)
+    val mtime: Long,       // Modified time (8 bytes)
+    val dev: Long,         // Device ID (8 bytes)
+    val ino: Long,         // Inode number (8 bytes)
+    val mode: Int,         // Mode (4 bytes)
+    val uid: Int,          // User ID (4 bytes)
+    val gid: Int,          // Group ID (4 bytes)
+    val sha: String?,      // SHA-1 hash (20 bytes)
+    val flags: Int,        // Flags (4 bytes)
+    val pathName: String   // Path + null terminator
+) {
+    fun serialize(file: RandomAccessFile, blobSha: String) {
+        file.writeLong(ctime)
+        file.writeLong(mtime)
+        file.writeLong(dev)
+        file.writeLong(ino)
+        file.writeInt(mode)
+        file.writeInt(uid)
+        file.writeInt(gid)
+        file.write(blobSha.toShaByte())
+        file.writeInt(flags)
+        file.writeBytes(pathName)
+        file.write(0)
+    }
+
+    companion object {
+        fun deserialize(buffer: ByteBuffer): IndexEntry {
+            val offset = buffer.position()
+            val ctime = buffer.getLong()
+            val mtime = buffer.getLong()
+            val dev = buffer.getLong()
+            val ino = buffer.getLong()
+            val mode = buffer.getInt()
+            val uid = buffer.getInt()
+            val gid = buffer.getInt()
+
+            val sha = ByteArray(20)
+            buffer.get(sha)
+
+            val flags = buffer.getInt()
+
+            val pathBuilder = StringBuilder()
+            var byte = buffer.get()
+            while (byte != 0.toByte()) {
+                pathBuilder.append(byte.toInt().toChar())
+                byte = buffer.get()
+            }
+            val pathName = pathBuilder.toString()
+
+            return IndexEntry(
+                offset, ctime, mtime, dev, ino,
+                mode, uid, gid, sha.toHexString(),
+                flags, pathName
+            )
+        }
+
+        fun getFileAttributes(path: Path): IndexEntry {
+            val attributes = Files.readAttributes(path, BasicFileAttributes::class.java)
+
+            val objectType: ObjectType = when {
+                Files.isSymbolicLink(path) -> ObjectType.SYMBOLIC_LINK
+                Files.isExecutable(path) -> ObjectType.EXECUTABLE_FILE
+                else -> ObjectType.REGULAR_FILE
+            }
+
+            return IndexEntry(
+                offset = null,
+                ctime = attributes.creationTime().toMillis(),
+                mtime = attributes.lastModifiedTime().toMillis(),
+                dev = Files.getAttribute(path, "unix:dev") as Long,
+                ino = Files.getAttribute(path, "unix:ino") as Long,
+                mode = objectType.mode.toInt(),
+                uid = Files.getAttribute(path, "unix:uid") as Int,
+                gid = Files.getAttribute(path, "unix:gid") as Int,
+                sha = null,
+                flags = 0,
+                pathName = path.toString()
+            )
+        }
+    }
+}
--- src/main/kotlin/com/zeljko/abstractive/zsv/manager/core/services/IndexService.kt
+++ src/main/kotlin/com/zeljko/abstractive/zsv/manager/core/services/IndexService.kt
@@ -1,28 +1,20 @@
 package com.zeljko.abstractive.zsv.manager.core.services
 
 import com.zeljko.abstractive.zsv.manager.core.objects.IndexEntry
-import com.zeljko.abstractive.zsv.manager.core.objects.ObjectType
 import com.zeljko.abstractive.zsv.manager.utils.FileUtils.INDEX_DIR
-import com.zeljko.abstractive.zsv.manager.utils.toShaByte
 import org.springframework.stereotype.Service
 import java.io.RandomAccessFile
 import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.nio.file.attribute.BasicFileAttributes
 
 
 @Service
 class IndexService(
     val blobService: BlobService
 ) {
-    val indexPath = Paths.get(INDEX_DIR)
-
-    enum class FileStatus {
-        MODIFY, ADD, REMOVE, CONFLICT, SAME
-    }
+    val indexPath: Path = Paths.get(INDEX_DIR)
 
     fun saveFileToIndex(filePath: Path) {
 
@@ -35,13 +27,24 @@ class IndexService(
             Files.write(indexPath, buffer.array())
         }
 
-        val indexEntry = getFileAttributes(filePath)
+        val indexEntry = IndexEntry.getFileAttributes(filePath)
+        val index = parseIndexFile(false)
 
-//        if (isFileInIndex()) {
-//
-//        }
+        index.firstOrNull {
+            it.pathName == indexEntry.pathName
+        }?.let { entry ->
+            if (entry.mtime != indexEntry.mtime) {
+                updateIndexEntry(indexEntry, entry.offset!!)
+            }
+        } ?: writeIndexEntry(filePath, indexEntry)
+    }
 
-        writeIndexEntry(filePath, indexEntry)
+    private fun updateIndexEntry(indexEntry: IndexEntry, offset: Int) {
+        RandomAccessFile(indexPath.toFile(), "rw").use { file ->
+            file.seek(offset.toLong())
+            val blobSha = blobService.compressFromFile(true, Paths.get(indexEntry.pathName))
+            indexEntry.serialize(file, blobSha)
+        }
     }
 
     private fun writeIndexEntry(filePath: Path, indexEntry: IndexEntry) {
@@ -55,50 +58,11 @@ class IndexService(
             file.seek(file.length())
 
             val blobSha = blobService.compressFromFile(true, filePath)
-
-            file.writeLong(indexEntry.ctime)
-            file.writeLong(indexEntry.mtime)
-            file.writeLong(indexEntry.dev)
-            file.writeLong(indexEntry.ino)
-            file.writeInt(indexEntry.mode)
-            file.writeInt(indexEntry.uid)
-            file.writeInt(indexEntry.gid)
-            file.write(blobSha.toShaByte())
-            file.writeInt(indexEntry.flags)
-            file.writeBytes(indexEntry.pathName)
-            file.write(0)
+            indexEntry.serialize(file, blobSha)
         }
     }
 
-    private fun isFileInIndex(): Boolean {
-        TODO("Not yet implemented")
-    }
-
-
-    fun getFileAttributes(path: Path): IndexEntry {
-        val attributes = Files.readAttributes(path, BasicFileAttributes::class.java)
-
-        val objectType: ObjectType = when {
-            Files.isSymbolicLink(path) -> ObjectType.SYMBOLIC_LINK
-            Files.isExecutable(path) -> ObjectType.EXECUTABLE_FILE
-            else -> ObjectType.REGULAR_FILE
-        }
-
-        return IndexEntry(
-            ctime = attributes.creationTime().toMillis(),
-            mtime = attributes.lastModifiedTime().toMillis(),
-            dev = Files.getAttribute(path, "unix:dev") as Long,
-            ino = Files.getAttribute(path, "unix:ino") as Long,
-            mode = objectType.mode.toInt(),
-            uid = Files.getAttribute(path, "unix:uid") as Int,
-            gid = Files.getAttribute(path, "unix:gid") as Int,
-            sha = null,
-            flags = 0,
-            pathName = path.toString()
-        )
-    }
-
-    fun parseIndexFile(): List<IndexEntry> {
+    fun parseIndexFile(print: Boolean = true): List<IndexEntry> {
         val indexEntries = mutableListOf<IndexEntry>()
         val bytes = indexPath.toFile().readBytes()
         val buffer = ByteBuffer.wrap(bytes)
@@ -111,40 +75,14 @@ class IndexService(
 
         val version = buffer.getInt()
         val entries = buffer.getInt()
-        println("Version of index file: $version")
-        println("Number of entries: $entries")
+        if (print) {
+            println("Number of entries: $entries")
+        }
 
         for (entry in 1..entries) {
-            val ctime = buffer.getLong()
-            val mtime = buffer.getLong()
-            val dev = buffer.getLong()
-            val ino = buffer.getLong()
-            val mode = buffer.getInt()
-            val uid = buffer.getInt()
-            val gid = buffer.getInt()
-
-            val sha = ByteArray(20)
-            buffer.get(sha)
-
-            val flags = buffer.getInt()
-
-            val pathBuilder = StringBuilder()
-            var byte = buffer.get()
-            while (byte != 0.toByte()) {
-                pathBuilder.append(byte.toInt().toChar())
-                byte = buffer.get()
-            }
-            val pathName = pathBuilder.toString()
-            indexEntries.add(
-                IndexEntry(
-                    ctime, mtime, dev, ino,
-                    mode, uid, gid, sha.toString(StandardCharsets.UTF_8),
-                    flags, pathName
-                )
-            )
+            indexEntries.add(IndexEntry.deserialize(buffer))
         }
+
         return indexEntries
     }
-
-
 }
